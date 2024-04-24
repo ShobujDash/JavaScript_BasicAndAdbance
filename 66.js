e143b7afcec6690b4ab8c6567d76966315383efd
ShobujDas <shobujd6.gmail.com>
2024-04-24 23:51:03 +0600

    new commit

---
+++ 66.js
@@ -0,0 +1,20 @@
+// iterables
+// jekhane ami for od loop korte pari
+// string , array are iterable
+
+// const firstName = "Harshit";
+// for (let char of firstName) {
+//   console.log(char)
+// }
+
+//
+
+
+// array like object
+// jetar modde length properting ache
+// r jetar modde index access korte pari
+// example :-string
+
+const firstName = "harshit";
+console.log(firstName.length);
+console.log(firstName[2]);
---
+++ 67.js
@@ -0,0 +1,9 @@
+// Sets (it is iterable)
+// store data
+// sets also habe its own methods
+// No index-based access
+// Order is not guaranteed 
+// unique items only (no duplicates allowed)
+
+
+
